2038b4bfbc20faad149c765e776a71fc38b36a6a
Rupal Gupta <rupalgupta99@gmail.com>
2023-05-29 13:19:35 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 100,
+              color: Colors.red,
+            ),
+            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              ),
+            ]),
+            Container(
+              width: 100,
+              color: Colors.blue,
+            ),
+          ],
+        )), //this is very similar to a div
       ),
-    ),
-  );
+    );
+  }
 }
